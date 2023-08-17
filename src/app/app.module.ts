e064010def1626eedc7c319d09afd3873a49b618
Salem Boyland <salemb@google.com>
2023-08-17 14:16:18 -0500

    updates to files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SeasonsDropdownComponent } from './seasons-dropdown/seasons-dropdown.component';
 import { EpisodeDropdownComponent } from './episode-dropdown/episode-dropdown.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
     SeasonsDropdownComponent,
-    EpisodeDropdownComponent
+    EpisodeDropdownComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
