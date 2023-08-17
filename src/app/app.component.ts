35ebba70bbf0f676d46acd5122f6ef175db57007
Mario Torres Jr <torres.mario590@gmail.com>
2023-08-17 14:54:27 -0500

    add service file to pull data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {OfficeApiService} from './office-api.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'office-quotes-angular';
+  constructor(private api:OfficeApiService) {
+
+  }
+  ngOnInit() {
+    this.api.apiCall().subscribe((data) => {
+      console.warn("get office api data", data);
+    })
+  }
 }
