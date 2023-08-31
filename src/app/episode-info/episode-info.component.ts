68c96facc9e75b485b84cd115d23b3c1b86a347b
Jared Chapman <deraj@google.com>
2023-08-31 11:43:56 -0500

    update episode-info.component.ts

--- src/app/episode-info/episode-info.component.ts
+++ src/app/episode-info/episode-info.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-episode-info',
     templateUrl: './episode-info.component.html',
-    styleUrls: ['./random-quote.component.css']
+    styleUrls: ['./episode-info.component.css']
 })
 export class EpisodeInfoComponent{}
