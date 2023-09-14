7b988079bd5a4db47d5b24be1f6e0d056d6c885f
Mario Torres Jr <mtorresjr@google.com>
2023-09-14 08:25:55 -0500

    modify testing files to pass api service and quote component

--- src/app/api-service/office-api.service.spec.ts
+++ src/app/api-service/office-api.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpTestingController, HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 import { OfficeApiService } from './office-api.service';
 
 describe('OfficeApiService', () => {
   let service: OfficeApiService;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ OfficeApiService ]
+    });
     service = TestBed.inject(OfficeApiService);
+    httpMock = TestBed.get(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient)
   });
 
   it('should be created', () => {
