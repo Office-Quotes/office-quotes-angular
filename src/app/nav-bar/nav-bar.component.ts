f79a1a5dbb792321bacaf3a52a11ecf6d5e58890
Jared Chapman <deraj@google.com>
2023-09-14 10:37:19 -0500

    fix bootstrap and components

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-nav-bar',
